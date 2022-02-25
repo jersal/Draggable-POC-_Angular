674470717eb071b49c63c7ebad6f07f5dbae2405
Muhammed Jersal (Harman Connected Services Inc) <v-mjersal@microsoft.com>
2022-02-25 12:02:33 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+declare  const PlainDraggable: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'drag-poc';
+  position:any;
+  
+ngOnInit(){
+
+
+    this.position = "(" + 373+", " + 123 +")";
+}
+  newPosition(event:any){
+    const boundingRect = event.currentTarget.getBoundingClientRect();
+    const element = event.currentTarget;
+    
+    // const x = event.pageX - boundingRect.left;
+    const x = element.offsetLeft;
+    const y = element.offsetTop;
+
+    this.position = "(" + x+", " + y +")";
+    console.log('yeah',x,y);
+  }
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularDraggableModule } from 'angular2-draggable';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, FormsModule, AngularDraggableModule],
+  declarations: [ AppComponent,  ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
